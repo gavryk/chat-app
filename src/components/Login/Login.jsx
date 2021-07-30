da086fc7eee9618fda2b28bfc68c0024618a885a
Gavryk <gvozdyk23@gmail.com>
2021-07-30 23:20:46 +0300

    create login page layout

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import style from './Login.module.scss'
+import { Google, Facebook } from "react-bootstrap-icons";
 
-function Login() {
+const Login = () => {
     return(
-        <div>
-            <h1>Hi, i'm login page</h1>
+        <div id={style.loginPage}>
+            <div className={`${style.loginCard} d-flex flex-wrap justify-content-center`}>
+                <h2 className='mb-4'>Welcome To BellaChat!</h2>
+                <div className={`${style.loginBtn} ${style.loginBtnGoogle}`}>
+                    <Google/> Sign In With Google
+                </div>
+                <div className={`${style.loginBtn} ${style.loginBtnFacebook}`}>
+                    <Facebook/> Sign In With Facebook
+                </div>
+            </div>
         </div>
     )
 }
