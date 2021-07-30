4698426dc16bcf7c070909f8a460617d5c526cf4
Gavryk <gvozdyk23@gmail.com>
2021-07-30 21:31:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import {Container} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-
+      <Container>
+        
+      </Container>
     </div>
   );
 }
